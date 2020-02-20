805d0bdaf94e35216e00231c2c857554d3283e01
Maciej Malecki <malecki.maciej@gmail.com>
2020-02-20 14:58:26 +0100

    Webflux spring boot app

---
+++ modules/adapters/backend-webflux/src/main/java/karbonfw/p4s/BackendWebfluxApplication.java
@@ -0,0 +1,12 @@
+package karbonfw.p4s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWebfluxApplication.class, args);
+    }
+}
